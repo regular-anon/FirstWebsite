3f96d0c67e1e8998264d2a278f2242cbd6674983
Cristian Balteanu <cristianbalt25@gmail.com>
2021-02-20 21:48:12 +0200

    INIT

---
+++ level.js
@@ -0,0 +1,7 @@
+﻿function foundWaldo() {
+    window.scrollTo(0, 0);
+    document.getElementById("endText").style = "background-color:aquamarine;border:dashed;text-align:center";
+    document.getElementById("endText").innerText = "✅Level Solved!";
+    alert("Congratulations! You found Waldo!")
+    return false;
+}
